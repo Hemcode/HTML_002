cbe535fc95fcc56e311d44c10ae72f0ae204cc1d
HemCode <kirbygamer905@gmail.com>
2022-08-30 10:45:56 +0200

    JavaScript-Online1

---
+++ script.js
@@ -0,0 +1,30 @@
+let resul;
+
+const plus = (x, y) => {
+	resul = x + y;
+	return resul;
+};
+
+const moin = (x, y) => {
+	resul = x - y;
+	return resul;
+};
+
+const foix = (x, y) => {
+	resul = x * y;
+	return resul;
+};
+
+const divi = (x, y) => {
+	resul = x / y;
+	return resul;
+};
+
+const puis = (x, y) => {
+	resul = x ** y;
+	return resul;
+};
+
+const reset = () => {
+	resul = 0;
+};
